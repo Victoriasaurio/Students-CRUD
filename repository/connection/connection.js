62ab4e23a04ac18ccbd42d4bbda968314b486ede
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-01-27 23:14:27 -0600

    Connection - Changes made to the connection file so that it can be used in the Jest file. - MySQL port was also added.

--- repository/connection/connection.js
+++ repository/connection/connection.js
@@ -1,18 +1,17 @@
 const mysql = require('mysql2/promise')
-let conexion = null
 
 async function connection() {
-    conexion = await mysql.createConnection({
+    return mysql.createConnection({
+        port: process.env.DB_PORT,
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_DATABASE
     })
-    return conexion
 }
 
 function getConnection () {
-    return conexion
+    return connection()
 }
 
 module.exports = {
