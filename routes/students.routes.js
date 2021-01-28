82e022c1db13247b8ae88c537f09349af155f832
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-01-28 13:16:44 -0600

    GET/:id - User search by added ID

--- routes/students.routes.js
+++ routes/students.routes.js
@@ -16,6 +16,24 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async(req, res) => {
+    try {
+        let id = req.params.id
+        let validId = validateId(id)
+        if(!validId) return res.status(400).send({status: 400, msg: 'Invalid id'})
+        let data = await Students.getStudents()
+        let valid_IDDB = validateIdDB(id, data)
+        if(!valid_IDDB) return res.status(400).send({status:400, msg: 'User ID does not exist'})
+        return res.status(200).send({
+            status: 200,
+            msg: 'Success',
+            data: data[0][id-1]
+        })
+    } catch(err) {
+        console.log(err)
+    }
+})
+
 router.post('/', async (req, res) => {
     try {
         let { body } = req
@@ -34,12 +52,12 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
     try {
-        let id = req.params
+        let id = req.params.id
         let {body} = req
-        let validId = validateId(id.id)
+        let validId = validateId(id)
         if(!validId) return res.status(400).send({status: 400, msg: 'Invalid id'})
         let data = await Students.getStudents()
-        let valid_IDDB = validateIdDB(id.id, data)
+        let valid_IDDB = validateIdDB(id, data)
         if(!valid_IDDB) return res.status(400).send({status:400, msg: 'User ID does not exist'})
         let valid = validate(body)
         if(!valid) return res.status(400).send({status: 400, msg: 'Invalid data (name o lastname)'})
@@ -47,7 +65,7 @@ router.put('/:id', async (req, res) => {
         return res.status(201).send({
             status: 201,
             msg: 'Updated student',
-            data: id
+            data: {"id":id}
         })
     } catch(err) {
         console.log(err)
@@ -56,17 +74,17 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
     try {
-        let id = req.params
-        let validId = validateId(id.id)
+        let id = req.params.id
+        let validId = validateId(id)
         if(!validId) return res.status(400).send({status: 400, msg: 'Invalid id'})
         let data = await Students.getStudents()
-        let valid_IDDB = validateIdDB(id.id, data)
+        let valid_IDDB = validateIdDB(id, data)
         if(!valid_IDDB) return res.status(400).send({status:400, msg: 'User ID does not exist'})
         await Students.deleteStudents(id)
         return res.status(200).send({
             status: 200,
             msg: 'Eliminated student',
-            data: id
+            data: {"id":id}
         })
     } catch(err) {
         console.log(err)
