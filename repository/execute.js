8524aa0c8ed44b3b2a1154140687b43090fcd6ca
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-01-27 23:19:09 -0600

    Execute - getConnection adapted because it is a function

--- repository/execute.js
+++ repository/execute.js
@@ -1,7 +1,11 @@
 const { getConnection } = require('./connection/connection')
 
 async function execute(sql) {
-    return await getConnection().execute(sql)
+    try {
+        return (await getConnection()).execute(sql) // Porque es una función - await getConnection()
+    } catch(err) {
+        console.log('Error on execute', err)
+    }
 }
 
 module.exports = {
